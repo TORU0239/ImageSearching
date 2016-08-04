40de645db7156938cca57557168800710476fdde
Toru Choi <toru@zikto.com>
2016-08-04 11:32:45 +0900

    searchview added

--- app/src/main/java/io/toru/imagesearching/MainActivity.java
+++ app/src/main/java/io/toru/imagesearching/MainActivity.java
@@ -1,15 +1,20 @@
 package io.toru.imagesearching;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +24,31 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar)findViewById(R.id.main_toolbar);
         toolbar.setTitle("KakaoTalk");
 
+        setSupportActionBar(toolbar);
+
         ViewPager vpPager = (ViewPager) findViewById(R.id.main_viewpager);
         vpPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
 
         TabLayout tab = (TabLayout) findViewById(R.id.main_tab_layout);
         tab.setupWithViewPager(vpPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        final SearchView searchView = (SearchView)menu.findItem(R.id.action_search).getActionView();
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 
     class ViewPagerAdapter extends FragmentStatePagerAdapter {
