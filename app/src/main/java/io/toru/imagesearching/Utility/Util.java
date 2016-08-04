a93140d3c648c4a212716f5e28eea50276e093dd
Toru Choi <toru@zikto.com>
2016-08-04 23:56:40 +0900

    api key and url

--- app/src/main/java/io/toru/imagesearching/Utility/Util.java
+++ app/src/main/java/io/toru/imagesearching/Utility/Util.java
@@ -9,7 +9,14 @@ import android.widget.EditText;
  */
 public class Util {
 
-    private static final String API_KEY = "b3b23c05a30783b9751b1b322d898718";
+    public static final String API_KEY = "b3b23c05a30783b9751b1b322d898718";
+    public static final String SEARCH_URL = "https://apis.daum.net/search/image";
+    public static final String API_KEY_PARAM = "apikey";
+    public static final String QUERY_PARAM = "q";
+    public static final String OUTPUT_PARAM= "output";
+
+//    https://apis.daum.net/search/image?apikey=b3b23c05a30783b9751b1b322d898718&q=다음카카오&output=json
+
 
     public static void hideKeyboard(Context ctx, EditText editText) {
         InputMethodManager imm = (InputMethodManager)ctx.getSystemService(Context.INPUT_METHOD_SERVICE);
